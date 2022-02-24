6f661a3b082da860b3ce90cbc097779fee34f92c
Gurpartap <sgurpartap880@gmail.com>
2022-02-24 14:07:15 -0500

    added new method

--- lecture/basic/ReverseNumber.java
+++ lecture/basic/ReverseNumber.java
@@ -6,15 +6,22 @@ public class ReverseNumber {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
-
-        int temp = n , revNum = 0;
-        while (temp > 0) {
-            int lastDigit = temp % 10;
-            temp = temp / 10;
-            revNum = revNum * 10 + lastDigit;
+        int ans = 0;
+        while (n > 0) {
+            int last = n % 10;
+            ans = ans * 10 + last;
+            n /= 10;
         }
+        System.out.println(ans);
 
-        System.out.println(revNum);
+//        int temp = n , revNum = 0;
+//        while (temp > 0) {
+//            int lastDigit = temp % 10;
+//            temp = temp / 10;
+//            revNum = revNum * 10 + lastDigit;
+//        }
+//
+//        System.out.println(revNum);
 
     }
 }
