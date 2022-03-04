ffe977399d409dcc33c5bce895214a945272b5af
Gurpartap <sgurpartap880@gmail.com>
2022-03-03 21:18:50 -0500

    fixed

--- assignment/function/Armstrong.java
+++ assignment/function/Armstrong.java
@@ -3,26 +3,5 @@ package assignment.function;
 import java.util.Scanner;
 
 public class Armstrong {
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int n = s.nextInt();
 
-        boolean ans = isArmStrong(n);
-        System.out.println(ans);
-    }
-
-    static boolean  isArmStrong(int n) {
-        int temp = n;
-        int sum = 0;
-        while (n > 0) {
-            int lastDigit = n % 10;
-            sum = sum + lastDigit * lastDigit * lastDigit;
-            n = n / 10;
-        }
-        if (sum == temp) {
-            return true;
-        }
-        else
-            return false;
-    }
 }
