d150823c3c785e57519b2c52993ff01ce2409561
Gurpartap <sgurpartap880@gmail.com>
2022-03-06 17:22:37 -0500

    Updated

---
+++ assignment/function/Factors.java
@@ -0,0 +1,30 @@
+package assignment.function;
+
+import java.util.Scanner;
+
+public class Factors {
+    public static void main(String[] args) {
+        // write a program to find factors of a number .
+        factors2();
+        factors2();
+    }
+    static void factors() {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0)
+                System.out.println(i);
+        }
+    }
+
+    static void factors2() {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0)
+                System.out.println(i);
+        }
+    }
+}
---
+++ assignment/function/SumProduct2.java
@@ -0,0 +1,35 @@
+package assignment.function;
+
+import java.util.Scanner;
+
+public class SumProduct2 {
+    public static void main(String[] args) {
+        sumOrProduct();
+        sumOrProduct();
+    }
+    static void sumOrProduct() {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();System.out.println("Enter 1 for sum and 2 for product : ");
+        int c = s.nextInt();
+
+
+        int sum = 0;
+        int product = 1;
+        if (c == 1){
+            for (int i = 1; i <= n; i++) {
+                sum += i;
+            }
+            System.out.println(sum);
+        }
+
+        else if(c == 2)
+        {
+            for (int i = 1; i <= n; i++) {
+                product *= i;
+            }
+            System.out.println(product);
+        }
+        else
+            System.out.println("-1");
+    }
+}
