021a73d47598244c4dcd9f120a9e288975c47c77
Gurpartap <sgurpartap880@gmail.com>
2022-03-10 21:50:10 -0500

    added lecture problems

---
+++ lecture/array/Max.java
@@ -0,0 +1,42 @@
+/*
+WAP a program to return MAX element of Array .
+ */
+package lecture.array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Max {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter size of array : ");
+        int size = in.nextInt();
+        int[] arr1 = new int[size];
+
+        for (int i = 0; i < arr1.length; i++) {
+            arr1[i] = in.nextInt();
+        }
+
+        System.out.println(Arrays.toString(arr1));
+
+        System.out.println(maxElementOfArray(arr1));
+
+
+    }
+
+    static int maxElementOfArray(int[] arr) {
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+}
+
+/*
+Edge cases
+ */
---
+++ lecture/array/Reverse.java
@@ -0,0 +1,42 @@
+package lecture.array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Reverse {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = s.nextInt();
+        }
+
+        System.out.println(Arrays.toString(arr));
+        reverseArray(arr);
+        System.out.println(Arrays.toString(arr));
+
+
+    }
+
+    static void reverseArray(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            //swap
+            swap(arr,start,end);
+                start++;
+                end--;
+
+
+        }
+    }
+
+    static void swap(int[] a,int s , int e) {
+        int t = a[s];
+        a[s] = a[e];
+        a[e] = t;
+    }
+}
