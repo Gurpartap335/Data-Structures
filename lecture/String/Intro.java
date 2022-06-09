cb5f5f5ca18ca3e4bd27e17f761f4fdd894a414a
Gurpartap Singh <gurpartap1258@chitkara.edu.in>
2022-06-09 13:30:14 -0400

    Strings lecture

---
+++ lecture/String/Intro.java
@@ -0,0 +1,56 @@
+package lecture.String;
+
+import java.util.Scanner;
+
+public class Intro {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+//        String a = "guri";
+//        String b = "guri";
+//        System.out.println(a == b);
+//        String c = "guriii";
+
+//        String A = new String("guri");
+//        String B = new String("guri");
+//        System.out.println(A == B);
+//        System.out.println(a.length());
+//        System.out.println(a.equals(b) + "  " + b.equals(c));
+//        System.out.println(a.charAt(0) + " " + a.charAt(1) + " " + a.charAt(2) + " " + a.charAt(3));
+//
+//        for (int i = 0; i < a.length(); i++) {
+//            System.out.print(a.charAt(i));
+//        }
+
+        // Pretty Printing in Java // place holder in java
+//        float ae = 43.34543f;
+//        System.out.printf("Formatted nuer is %.2f", ae);
+
+        // String builder
+
+        // String methods
+
+
+
+    }
+}
+/*
+A string variable contains a collection of characters surrounded by double quotes.
+
+String    name     =  "guri"
+datatype  ref.var     object
+
+Internal working of string
+String Pool  separate memory
+immutability
+ */
+
+/*
+The java command-line argument is an argument i.e. passed at the time of running the java program.
+
+String[] args
+String[] -> simple String array
+args -> name of the array it can be anything(a,ar, parameter)
+
+when would you these args ?
+ */
---
+++ lecture/String/Operator.java
@@ -0,0 +1,23 @@
+package lecture.String;
+
+import java.util.ArrayList;
+
+public class Operator {
+    public static void main(String[] args) {
+        // String Concatenation operator
+//        System.out.println('a' + 'b');
+//        System.out.println("a" + "b");
+//        System.out.println('a' + "b");
+//        System.out.println('a' + 3);
+//        System.out.println("a" + 3);
+//
+//        System.out.println((char)('a' + 3) + " " + ('a' + 3));
+
+        System.out.println("Kunal" + new ArrayList<>()); // call to toString method for this
+        System.out.println("kunal" + new Integer(44));
+        System.out.println("dfdf" + new String("fdg dkfmdlf"));
+    }
+}
+/*
+
+ */
