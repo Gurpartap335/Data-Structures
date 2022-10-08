e1b20181edfde494c5040cce22c738469ba98901
Gurpartap Singh <sgurpartap880@gmail.com>
2022-10-08 19:47:39 +0530

    Two Sum

--- leetcode/array/TwoSum.java
+++ leetcode/array/TwoSum.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
+// https://leetcode.com/problems/two-sum/
+
 public class TwoSum {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
@@ -20,41 +22,81 @@ public class TwoSum {
         System.out.println(Arrays.toString(nums));
 
         int target = s.nextInt();
-        System.out.println(Arrays.toString(twoSum2(nums,target)));
+        System.out.println(Arrays.toString(twoSumHashMap(nums,target)));
     }
 
-//    static int[] twoSum(int[] nums, int target) {
-//        int[] n = new int[2];
-//
-//        for (int i = 0; i < nums.length; i++) {
-//
-//            for (int j = 1; j < nums.length; j++) {
-//                if (i == j || i > j) {
-//                    continue;
-//                }
-//                else if (nums[i] + nums[j] == target) {
-//                    n[0] = i;
-//                    n[1] = j;
-//                    return n;
-//                }
-//            }
-//        }
-//        return n;
-//    }
-
-    static int[] twoSum2(int[] nums, int target) {
+    // brute force approach. Two nested loops
+    static int[] twoSum(int[] nums, int target) {
         int[] n = new int[2];
 
-        Map<Integer,Integer> map = new HashMap<>();
-
         for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(target - nums[i])) {
-                n[1] = i;
-                n[0] = map.get(target - nums[i]);
-                return n;
+
+            for (int j = i + 1; j < nums.length; j++) {
+
+                if (nums[i] + nums[j] == target) {
+                    n[0] = i;
+                    n[1] = j;
+                    return n;
+                }
             }
-            map.put(nums[i],i);
         }
         return n;
     }
+
+    static int[] twoSumHashMap(int[] nums, int target) {
+
+        HashMap<Integer, Integer> ans = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (ans.containsKey(target - nums[i])) {
+                return new int[] {ans.get(target - nums[i]), i}; // get method return the value to which specified key is mapped.
+            }
+            ans.put(nums[i], i); // store nums array in HashMap object ans. key as nums element and  their index as value.
+        }
+        return null;
+    }
+
 }
+
+
+// HashTable is obsolete in Java 1.7 and it is recommended to use ConcurrentMap implementation.
+// Hashtable is slower than Hashmap.
+// HashTable does not allow null keys or values.
+// HashMap allows one null key and any number of null values.
+
+class LearningHashMap{
+    static HashMap<String, Integer> phonebook = new HashMap<String, Integer>();
+
+    public static void main(String[] args) {
+        phonebook.put("Ken", 54344334);
+        phonebook.put("Ye", 2);
+        phonebook.put("Nav", 2343232);
+        phonebook.put("Drake", 123432);
+        phonebook.put("Dua", null);
+
+        System.out.println(phonebook);
+
+        phonebook.put("Cat", 3423400);
+
+        System.out.println(phonebook.get("Ye"));
+        System.out.println(phonebook.get(123432));
+        System.out.println(phonebook.get(2));
+        System.out.println(phonebook.get("Nav")); // return object key
+        System.out.println(phonebook.keySet()); // random order
+        System.out.println(phonebook.entrySet()); // random order
+        System.out.println(phonebook.containsKey("Ye"));
+        System.out.println(phonebook.containsKey("ERe"));
+        System.out.println(phonebook.containsValue(0));
+        System.out.println(phonebook.isEmpty());
+        System.out.println(phonebook.size());
+
+        System.out.println(phonebook.get("Drake"));
+
+    }
+}
+
+
+
+
+
+
