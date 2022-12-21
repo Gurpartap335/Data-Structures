2d0676b65ffbe88bb491f9774b748707754a2861
Gurpartap Singh <sgurpartap880@gmail.com>
2022-12-21 13:57:02 +0530

    add Doubly linked list

---
+++ lecture/linkedList/DLL.java
@@ -0,0 +1,108 @@
+package lecture.linkedList;
+
+public class DLL {
+    private Node head;
+    private int size;
+
+    void insertFirst(int val) {
+        Node node = new Node(val);
+        node.next = head;
+        node.prev = null;
+        if (head != null) {
+            head.prev = node;
+        }
+        head = node;
+        size++;
+     }
+
+    void insertLast(int val) {
+        Node node = new Node(val);
+        Node last = head;
+
+        node.next = null;
+
+        if (head == null) {
+            node.prev = null;
+            head = node;
+            return;
+        }
+
+        while (last.next != null) {
+            last = last.next;
+        }
+
+        last.next = node;
+        node.prev = last;
+    }
+    
+    public Node find(int value) {
+        Node node = head;
+        while (node != null) {
+            if (node.value == value) {
+                return node;
+            }
+            node = node.next;
+        }
+        return null;
+    }
+
+    public void insert(int after, int val) {
+        Node p = find(after);
+
+        if (p == null) {
+            System.out.println("Does not exist");
+            return;
+        }
+        Node node = new Node(val);
+        node.next = p.next;
+        p.next = node;
+        node.prev = p;
+        if (node.next != null) {
+            node.next.prev = node;
+        }
+
+    }
+    public void display() {
+        Node node = head;
+        Node last = null;
+        while (node != null) {
+            System.out.print(node.value + " -> ");
+            last = node;
+            node = node.next;
+        }
+        System.out.println("End");
+//
+//        System.out.println("Print in reverse");
+//        while (last != null) {
+//            System.out.print(last.value + " -> ");
+//            last = last.prev;
+//        }
+//        System.out.println("Start");
+    }
+
+    private class Node {
+        private int value;
+        private Node next;
+        private Node prev;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public Node(int value, Node next, Node prev) {
+            this.value = value;
+            this.next = next;
+            this.prev = prev;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        DLL list = new DLL();
+        list.insertFirst(12);
+        list.insertFirst(14);
+        list.insertFirst(10);
+        list.insertFirst(100);
+        list.display();
+    }
+}
