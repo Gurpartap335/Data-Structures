4edf772f4a21f1bd35b820f0aee6f7ca5c70b921
Gurpartap Singh <sgurpartap880@gmail.com>
2023-02-11 20:53:27 +0530

    add Linked List

---
+++ lecture/linkedList/practise/Main.java
@@ -0,0 +1,61 @@
+package lecture.linkedList.practise;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+//        SinglyLinkedList<Integer> list = new SinglyLinkedList();
+//        list.addLast(1);
+//        list.addLast(4);
+//        list.addLast(9);
+//        list.addLast(23);
+//        list.printList();
+//        System.out.println(list.getSize());
+//        list.addLast(55);
+//        list.printList();
+//        System.out.println(list.getSize());
+//        list.addFirst(31);
+//        list.printList();
+//
+//        list.delete(4);
+//        list.delete(1);
+//        list.printList();
+//        list.delete(0);
+//        list.printList();
+//        list.delete(2);
+//        list.printList();
+//        list.delete(1);
+//        list.printList();
+//        list.delete(0);
+//        list.printList();
+//
+//        list.addFirst(1);
+//        list.addFirst(2);
+//        list.addFirst(3);
+//        list.addFirst(6);
+//        list.addFirst(5);
+//        list.printList();
+//       System.out.println(list.getSize());
+//        list.delete(0);
+//        list.delete(0);
+//        list.delete(0);
+//        list.delete(0);
+//        list.printList();
+//        list.delete(0);
+//        list.printList();
+//
+//        System.out.println(list.search(3));
+//        System.out.println(list.search(99));
+
+
+        DoublyLinkedList<Integer> list = new DoublyLinkedList<>();
+        list.addFirst(33);
+        list.addFirst(323);
+        list.addFirst(22);
+        list.printList();
+
+
+    }
+
+
+}
