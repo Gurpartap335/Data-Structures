c1c7780be902008560daeaed4256350dfe67569c
Gurpartap Singh <sgurpartap880@gmail.com>
2023-09-08 21:47:59 +0530

    added recursive sort method

--- src/main/java/lecture/sorting/BubbleSort.java
+++ src/main/java/lecture/sorting/BubbleSort.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 public class BubbleSort {
     public static void main(String[] args) {
         BubbleSort sort = new BubbleSort();
-        int[] arr= {6, 2, 8, 4, 10};
-        sort.bubbleSort(arr);
+        int[] arr= {5, 4, 3, 2, 1, 0};
+        sort.recursiveBubbleSort(arr, arr.length - 1);
         System.out.println(Arrays.toString(arr));
 
     }
@@ -33,6 +33,34 @@ public class BubbleSort {
         arr[i] = arr[j];
         arr[j] = temp;
     }
+
+
+
+
+    // TC : O(n^2) O(n) best case
+    // SC : O(n). SC is equal to the recursion stack.
+    public void recursiveBubbleSort(int[] arr, int n) {
+        if (n == 0) {
+            return;
+        }
+
+        boolean swapped = false;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > arr[i + 1]) {
+                swap(arr, i, i + 1);
+                swapped = true;
+            }
+        }
+
+        if (!swapped) {
+            return;
+        }
+        recursiveBubbleSort(arr, n - 1);
+
+    }
+
+
+
 
     // recursive
     // r : arr.length - 1 for array getting out of bound
--- src/main/java/lecture/sorting/InsertionSort.java
+++ src/main/java/lecture/sorting/InsertionSort.java
@@ -6,8 +6,8 @@ import java.util.Scanner;
 public class InsertionSort {
     public static void main(String[] args) {
         InsertionSort sort = new InsertionSort();
-        int[] arr = {1, 2, 3, 4, 5};
-        sort.insertionSort(arr);
+        int[] arr = {5, 4, 3, 2, 1};
+        sort.recursiveInsertionSort(arr, 0);
         System.out.println(Arrays.toString(arr));
 
     }
@@ -44,10 +44,27 @@ public class InsertionSort {
         arr[index2] = temp;
     }
 
+    // TC : O(n^2) for loop + n times function get called
+    // SC : O(n)
+    public void recursiveInsertionSort(int[] arr, int i) {
+        if (i == arr.length - 1) {
+            return;
+        }
+        for (int j = i + 1; j > 0; j--) {
+            if (arr[j - 1] > arr[j]) {
+                swap(arr, j, j - 1);
+            } else {
+                break;
+            }
+        }
+
+        recursiveInsertionSort(arr, i + 1);
+    }
+
 }
 /*
 Takes an element and place it in its current position.
 
-TC : O(n^2) Best Case O(1)
+TC : O(n^2) Best Case O(n)
 SC: O(1)
  */
