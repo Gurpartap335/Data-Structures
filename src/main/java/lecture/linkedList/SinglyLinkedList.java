0be96153d8ee0a4bdfbf61ddbbd6de41033f6566
Gurpartap Singh <sgurpartap880@gmail.com>
2023-12-10 14:20:33 +0530

    Update SinglyLinkedList.java

--- src/main/java/lecture/linkedList/SinglyLinkedList.java
+++ src/main/java/lecture/linkedList/SinglyLinkedList.java
@@ -1,149 +1,93 @@
 package lecture.linkedList;
 
-import java.util.Stack;
+// Implemented Linked List using both Head and Tail pointer.
+public class SinglyLinkedList {
 
-// without tail pointer
-class SinglyLinkedList<E> {
     private Node head;
-
+    private Node tail;
     private int size;
-
     public SinglyLinkedList() {
         head = null;
+        tail = null;
         size = 0;
     }
 
-
-    public boolean isEmpty() {
-        return size == 0;
-    }
-
-    public int size() {
+    public int getSize() {
         return size;
     }
 
-    public void insertFirst(E value) {
-        Node node = new Node(value);
-        // System.out.println("Node next value: " + node.next);
-        if (head == null) {
-            head = node;
-            size++;
-            return;
-        }
-
+    // O(1)
+    public void insertFirst(int val) {
+        Node node = new Node(val);
         node.next = head;
         head = node;
+
+        if (tail == null) {
+            tail = head;
+        }
         size++;
     }
 
-    public void insertLast(E value) {
+    public void insertLast(int val) {
+        if (tail == null) {
+            insertFirst(val);
+            return;
+        }
+        Node node = new Node(val);
+        tail.next = node;
+        tail = node;
+        size++;
+    }
+
+    // using head pointer O(n)
+    public void insertLast2(int value) {
         Node node = new Node(value, null);
-        if (head == null) {
-            insertFirst(value);
-            return;
-        }
 
         Node temp = head;
         while (temp.next != null) {
             temp = temp.next;
         }
         temp.next = node;
+
+    }
+
+    public void insert(int index, int val) {
+        if (index == 0) {
+            insertFirst(val);
+            return;
+        }
+        if (index == size) {
+            insertLast(val);
+            return;
+        }
+        Node temp = head;
+        for (int i = 1; i < index; i++) {
+            temp = temp.next;
+        }
+
+        temp.next = new Node(val, temp.next);
         size++;
     }
 
-    public E getFirst() {
-        return head.value;
-    }
+    // insert using recursion
+    // void return type and make changes in ll
+    // node return type that returns the list node to change the structure
 
-    public E getLast() {
-        Node temp = head;
-        while (temp.next != null) {
-            temp = temp.next;
+    public void insertRec(int value, int index) {
+        if (index > size) {
+            return;
         }
-        return temp.value;
+        head = insertRec(value, index, head);
     }
-
-    public E removeFirst() {
-        Node temp = head;
-        head = head.next;
-        size--;
-        return temp.value;
-    }
-
-    public E removeLast() {
-        if (size == 1) {
-            return removeFirst();
-        }
-
-        Node temp = head;
-        while (temp.next.next != null) {
-            temp = temp.next;
-        }
-        Node LastNode = temp.next;
-        temp.next = null;
-        size--;
-        return LastNode.value;
-    }
-
-    public void add(E value, int index) {
+    public Node insertRec(int val, int index, Node node) {
         if (index == 0) {
-            insertFirst(value);
-            return;
-        }
-        if (index == size - 1) {
-            insertLast(value);
-            return;
+            Node temp = new Node(val, node);
+            size++;
+            return temp;
         }
 
-        Node temp = head;
-        for (int i = 1; i < index; i++) { // insert at index not after index
-            temp = temp.next;
-        }
-
-        Node node = new Node(value, temp.next);
-        temp.next = node;
-        size++;
-    }
-
-    public Node get(int index) {
-        Node temp = head;
-        for (int i = 0; i < index; i++) {
-            temp = temp.next;
-        }
-        return temp;
-    }
-
-    public void remove(int index) {
-        if (index == 0) {
-            removeFirst();
-            return;
-        }
-        if (index == size - 1) {
-            removeLast();
-            return;
-        }
-
-        Node prev = get(index - 1);
-        prev.next = prev.next.next;
-        size--;
-    }
-
-    /**
-     * Test if the value key is present in the list.
-     * @param key the value to be searched
-     * @return {@code true} if key is present in the list, otherwise
-     * {@code false}
-     */
-    public boolean search(E value) {
-        Node temp = head;
-
-        for (int i = 0; i < size; i++) {
-            if (temp.value == value) {
-                return true;
-            }
-            temp = temp.next;
-        }
-        return false;
+        node.next = insertRec(val, index - 1, node.next);
+        return node;
     }
 
     @Override
@@ -158,66 +102,112 @@ class SinglyLinkedList<E> {
             sb.append(temp.value).append(" -> ");
             temp = temp.next;
         }
-        sb.append("null");
+        sb.append("End");
         return sb.toString();
     }
 
-    public void clear() {
-        Node current = head;
-        while (current != null) {
-            Node prev = current;
-            current = current.next;
-            prev = null;
+    public void deleteFirst() {
+        head = head.next; // just move head by once
+        if (head == null) {
+            tail = null;
         }
-        head = null;
-        size = 0;
+        size--;
     }
 
-    // https://leetcode.com/problems/remove-duplicates-from-sorted-list/
-    public void removeDuplicate(SinglyLinkedList list) {
-       Node node = head;
-       while (node.next != null) {
-           if (node.value == node.next.value) {
-               node.next = node.next.next; // we moved node.next value not node.
-               size--;
-           } else {
-               node = node.next;
-           }
-       }
+    public void deleteLast() {
+        if (size <= 1) {
+            deleteFirst();
+            return;
+        }
+        Node secondLast = get(size - 2);
+        tail = secondLast;
+        tail.next = null;
+    }
+
+    public void delete(int index) {
+        if (index == 0) {
+            deleteFirst();
+            return;
+        }
+
+        if (index == size - 1) {
+            deleteLast();
+            return;
+        }
+        Node prev = get(index - 1);
+        prev.next = prev.next.next;
+        size--;
+    }
+
+    public Node get(int index) {
+        Node node = head;
+        for (int i = 0; i < index; i++) {
+            node = node.next;
+        }
+        return node;
     }
 
 
-    // nested or inner class
-    // https://www.digitalocean.com/community/tutorials/java-inner-class
-    // assert keyboard
-    public class Node {
-        private Node next;
-        private E value;
+    public Node find(int value) {
+        Node node = head;
+        while (node != null) {
+            if (node.value == value) {
+                return node;
+            }
+            node = node.next;
+        }
+        return null;
+    }
 
-        public Node(Node next) {
-            this.next = next;
+    // questions
+    // sorted list; remove duplicate elements
+    public void duplicate() {
+        Node node = head;
+        while (node.next != null) {
+            if (node.next != null && node.value == node.next.value) {
+                node.next = node.next.next;
+                size--;
+            } else {
+                node = node.next;
+            }
         }
 
-        public Node(E value) {
-            this.value = value;
+        tail = node;
+        tail.next = null;
+    }
+
+
+    // https://leetcode.com/problems/remove-linked-list-elements/
+    public void removeElements(SinglyLinkedList list, int val) {
+        Node curr = head;
+        if (head.value == val) {
+            head = head.next;
+            size--;
         }
 
-        public Node(Node next, E value) {
-            this.next = next;
-            this.value = value;
-        }
-
-        public Node(E value, Node next) {
-            this.next = next;
-            this.value = value;
+        while (curr.next != null) {
+            if (curr.next.value == val) {
+                curr.next = curr.next.next;
+                size--;
+            } else {
+                curr = curr.next;
+            }
         }
     }
 
-    public Node getHead() {
-        return head;
+
+    private void reverse(Node node) {
+        if (node.next == null) {
+            head = tail;
+            return;
+        }
+        reverse(node.next);
+        tail.next = node;
+        tail = node;
+        tail.next = null; // so last node will point to null.
     }
 
-    // Create and detect loop
+
     public void createLoop(Node head, int k) {
         if (head == null) {
             return;
@@ -238,214 +228,31 @@ class SinglyLinkedList<E> {
         temp.next = connectedPoint;
     }
 
-    public E getNodeValue(int index) {
-        Node temp = head;
-        for (int i = 0; i < index; i++) {
-            temp = temp.next;
-        }
-        return temp.value;
-    }
+    private class Node {
+        private int value;
+        private Node next; // reference variable
 
-    public boolean isPalindrome(SinglyLinkedList<E> list) {
-        int i = 0;
-        int j = size - 1; //  size variable is not provided in leetcode problems
-        while (i <= j) { // <= for odd length list.
-            if (getNodeValue(i) != getNodeValue(j)) {
-                return false;
-            }
-            i++;
-            j--;
-        }
-        return true;
-    }
-
-    public boolean isPalindromeStack(SinglyLinkedList<E> list) {
-        Node fast = head;
-        Node slow = head;
-
-        Stack stack = new Stack<>();
-
-        while(fast!=null && fast.next!=null){
-            stack.push(slow.value);
-            fast = fast.next.next;
-            slow = slow.next;
-
+        public Node(int value) {
+            this.value = value;
         }
 
-        if(fast != null)
-            slow = slow.next;
-
-        while(slow != null){
-            if (stack.pop() != slow.value) return false;
-            slow = slow.next;
+        public Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
         }
-        return true;
-    }
-
-
-
-    // is linked list Palindrome using Two pointers
-    public boolean isPalindrome2(SinglyLinkedList<E> list) {
-        Node fast = head;
-        Node slow = head, temp;
-
-        while (fast != null && fast.next != null) {
-            fast = fast.next.next;
-            slow = slow.next;
-        }
-
-        Node prev = slow;
-        slow = slow.next;
-        prev.next = null;
-        while (slow != null) {
-            temp = slow.next;
-            slow.next = prev;
-            prev = slow;
-            slow = temp;
-        }
-        fast = head;
-        slow = prev; // first node of the reversed part
-        while (slow != null) {
-            if (slow.value != fast.value) {
-                return false;
-            }
-            slow = slow.next;
-            fast = fast.next;
-        }
-        return true;
-    }
-    public  boolean detectLoop(Node head) {
-        Node fast = head;
-        Node slow = head;
-
-        while (fast != null && fast.next != null) {
-            fast = fast.next.next;
-            slow = slow.next;
-            if (fast == slow) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    // https://leetcode.com/problems/remove-linked-list-elements/
-    // why use dummy node find in this question.
-//    public void removeElements(SinglyLinkedList<E> list, int value) {
-//        if (head != null && head.value == value) {
-//            head = head.next;
-//        }
-//        Node curr = head;
-//
-//        while (curr.next != null) {
-//            if (curr.next.value == value) {
-//                curr = curr.next.next;
-//            } else {
-//                curr.next = curr.next.next;
-//            }
-//        }
-//    }
-
-    // reverse Linked list
-    // iterative method
-    // after reversing the whole list next and curr will be pointing to null and prev
-    // pointer will be at the first node of the linked list
-    // You will not use extra memories unless you use new keyword.
-    // All java objects live in java heap.
-    // O(n) O(1)
-    public void reverse(SinglyLinkedList<E> list) {
-        if (list.head == null) {
-            System.out.println("List is empty");
-            return;
-        }
-        if (size < 2) {
-            return;
-        }
-        Node prev = null;
-        Node curr = head;
-        Node next = curr.next;
-
-        while (curr != null) {
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-            if (next != null) { // to escape NullPointerException
-                next = next.next; // cannot read field next because next is null.
-            }
-           }
-        head = prev;
-    }
-
-    // recursion reverse linked list
-    // O(n) O(n)
-    // reach the end of LL and start reversing the linked list when I move out of the recursion.
-    public void reverseRec(Node node) {
-        int i = 1;
-        Node tail = null;
-        if (i == 1) {
-            tail = get(size - 1);
-            i++;
-        }
-
-        if (node.next == null) {
-            head = tail;
-            return;
-        }
-        reverseRec(node.next);
-        tail.next = node;
-        tail = node;
-        tail.next = null;
-    }
-
-    // return list from the middle of the linked list
-    // https://leetcode.com/problems/middle-of-the-linked-list/
-    public E middleElement(SinglyLinkedList<E> list) {
-        Node fast = list.head;
-        Node slow = list.head;
-
-        while (fast != null && fast.next != null) {
-            fast = fast.next.next;
-            slow = slow.next;
-        }
-        return slow.value;
-    }
-
-    public SinglyLinkedList<E> middleList(SinglyLinkedList<E> list) {
-        Node fast = list.head;
-        Node slow = list.head;
-
-        SinglyLinkedList<E> ans = new SinglyLinkedList<E>();
-
-        while (fast != null && fast.next != null) {
-            fast = fast.next.next;
-            slow = slow.next;
-        }
-
-        while (slow != null) {
-            ans.insertLast(slow.value);
-            slow = slow.next;
-        }
-        return ans;
-    }
-
-    // to check what comes after null??
-    public void nullNext() {
-        Node temp = null;
-        System.out.println(temp.next);
     }
 
     public static void main(String[] args) {
-        SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
-
-        list.insertLast(1);
-        list.insertLast(2);
-        list.insertLast(3);
-        list.insertLast(4);
-        list.insertLast(3);
-        list.insertLast(2);
-        list.insertLast(1);
-        list.clear();
-//        System.out.println(list.isPalindrome2(list));
+        SinglyLinkedList list1 = new SinglyLinkedList();
+        list1.insertLast(6);
+        list1.insertLast(1);
+        list1.insertLast(2);
+        list1.insertLast(6);
+        list1.insertLast(3);
+        list1.insertLast(4);
+        list1.insertLast(6);
+        list1.insertLast(5);
+        list1.reverse(list1.head);
 
     }
 }
-
---
+++ src/main/java/leetcode/linkedList/SinglyLinkedList.java
@@ -0,0 +1,242 @@
+package leetcode.linkedList;
+
+
+public class SinglyLinkedList {
+
+    public Node head; // pointer
+
+    private int size;
+    public SinglyLinkedList() {
+        head = null;
+        size = 0;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+
+    public void addFirst(int value) {
+        head = new Node(value, head);
+        size++;
+    }
+
+    public void addLast(int value) {
+        Node node = new Node(value, null);
+        if (head == null) {
+            head = node;
+            size++;
+            return;
+        }
+
+        Node temp = lastNode();
+        temp.next = node;
+        size++;
+    }
+
+
+    public void addAtIndex(int index, int value) {
+        if (0 > index || index > size) {
+            System.out.println("Invalid Index");
+            return;
+        }
+        if (index == 0) {
+            addFirst(value);
+            return;
+        }
+        if (index == size) {
+            addLast(value);
+            return;
+        }
+        Node temp = head;
+        for (int i = 0; i < index - 1; i++) {
+            temp = temp.next;
+        }
+
+        Node node = new Node(value, temp.next);
+        temp.next= node;
+        size++;
+    }
+
+    public void add(int value) {
+        addLast(value);
+    }
+
+    public int getFirst() {
+        return head.value;
+    }
+
+    public int getLast() {
+        Node last = lastNode();
+        return last.value;
+    }
+
+    public int getAtIndex(int index) {
+        if (index < 0 || index >= size) {
+            System.out.println("Invalid Index");
+            return -1;
+        }
+        if (index == 0) {
+            return getFirst();
+        }
+
+        if (index == size - 1) {
+            return getLast();
+        }
+
+        Node temp = head;
+        for (int i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+        return temp.value;
+    }
+
+    public int removeFirst() {
+        Node temp = head;
+        head = head.next;
+        temp.next = null;
+        size--;
+        return temp.value;
+    }
+
+    public int removeLast() {
+        Node secondLast = head;
+        while (secondLast.next.next != null) {
+            secondLast = secondLast.next;
+        }
+        Node last = secondLast.next;
+        secondLast.next = null;
+        size--;
+        return last.value;
+    }
+
+    public int remove(int index) {
+        if (index == 0) {
+            return removeFirst();
+        }
+        if (index == size - 1) {
+            return removeLast();
+        }
+
+        Node prev = getNode(index - 1);
+        Node removeNode = getNode(index);
+        prev.next = prev.next.next;
+        removeNode.next = null;
+        size--;
+        return removeNode.value;
+    }
+
+    public Node getNode(int index) {
+        Node temp = head;
+        for (int i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+        return temp;
+    }
+
+    public Node lastNode() {
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        return temp;
+    }
+
+    public void clear() {
+        Node current = head;
+        while (current != null) {
+            Node prev = current;
+            current = current.next;
+            prev.next = null;
+        }
+        head = null;
+        size = 0;
+    }
+
+    public boolean search(int value) {
+        Node temp = head;
+
+        while (temp != null) {
+            if (temp.value == value) {
+                return true;
+            }
+            temp = temp.next;
+        }
+        return false;
+    }
+
+    public void display() {
+        Node node = head;
+        while (node != null) {
+            System.out.print(node.value + " -> ");
+            node = node.next;
+        }
+        System.out.println("null");
+    }
+
+
+    @Override
+    public String toString() {
+        Node temp = head;
+        if (temp == null) {
+            return "[]";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (temp != null) {
+            sb.append(temp.value).append(" -> ");
+            temp = temp.next;
+        }
+        sb.append("null");
+        return sb.toString();
+    }
+
+
+    public static void main(String[] args) {
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.add(1);
+        list.add(3);
+        list.add(4);
+        list.add(7);
+        list.add(1);
+        list.add(4);
+        list.add(6);
+        System.out.println(list);
+
+    }
+
+
+
+}
+
+
+// A node only knows about what data it contains, and who its neighbor is
+class Node {
+    int value;
+    Node next;
+
+    public Node() {
+
+    }
+    public Node(int data) {
+        this.value = data;
+    }
+
+    public Node(int data, Node next) {
+        this.value = data;
+        this.next = next;
+    }
+
+    public void print(Node node) {
+        while (node != null) {
+            System.out.print(node.value + " -> ");
+            node = node.next;
+        }
+        System.out.println("null");
+    }
+
+}
