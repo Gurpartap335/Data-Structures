60650028f3f581a16e08fac83792983a0a60898c
Gurpartap Singh <sgurpartap880@gmail.com>
2024-03-05 14:48:26 +0530

    add Union and Intersection

---
+++ src/main/java/lecture/array/UnionAndIntersection.java
@@ -0,0 +1,122 @@
+package lecture.array;
+
+import java.util.*;
+
+public class UnionAndIntersection {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 1, 1, 1, 2, 3, 4, 9, 11, 14};
+        int[] arr2 = {1, 1, 1, 2, 2, 2, 3, 4, 10, 12, 13};
+
+        System.out.println(Arrays.toString(union(arr1, arr2)));
+        System.out.println(Arrays.toString(union2(arr1, arr2)));
+
+    }
+
+    // O(n log n), O(n) set + O(n) array. n : size of arr1 and arr2
+    public static int[] union(int[] arr1, int[] arr2) {
+        TreeSet<Integer> set = new TreeSet<>();
+
+        for (int j : arr1) {
+            set.add(j);
+        }
+
+        for (int i : arr2) {
+            set.add(i);
+        }
+
+        int[] arr3 = new int[set.size()];
+
+        int i = 0;
+        for (int a : set) {
+            arr3[i++] = a;
+        }
+
+        return arr3;
+    }
+
+
+    // O(k), O(k) for returning the answer not for solving
+    // k is size of arr1 + size of arr2.
+    public static int[] union2(int[] arr1, int[] arr2) {
+        int i = 0, j = 0;
+        ArrayList<Integer> list = new ArrayList<>();
+
+        while (i < arr1.length && j < arr2.length) {
+            if (arr1[i] == arr2[j]) {
+                if (list.size() == 0 || arr1[i] != list.get(list.size() - 1)) {
+                    list.add(arr1[i]);
+                }
+                i++;
+                j++;
+            }
+            else if (arr1[i] < arr2[j]) {
+                if (list.size() == 0 || arr1[i] != list.get(list.size() - 1)) {
+                    list.add(arr1[i]);
+                }
+                i++;
+            } else {
+                if (list.size() == 0 || arr2[j] != list.get(list.size() - 1)) {
+                    list.add(arr2[j]);
+                }
+                j++;
+            }
+        }
+
+        while (i < arr1.length) {
+            if (arr1[i] != list.get(list.size() - 1)) {
+                list.add(arr1[i]);
+            }
+            i++;
+        }
+
+        while (j < arr2.length) {
+            if (arr2[j] != list.get(list.size() - 1)) {
+                list.add(arr2[j]);
+            }
+            j++;
+        }
+
+        int[] arr3 = new int[list.size()];
+
+        int a = 0;
+        for (int e : list) {
+            arr3[a++] = e;
+        }
+
+        return arr3;
+    }
+
+
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+
+        Set<Integer> set = new HashSet<>();
+        int i = 0, j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] == nums2[j]) {
+                set.add(nums1[i]);
+                i++;
+                j++;
+            }
+            else if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+
+        int[] arr = new int[set.size()];
+        int a = 0;
+        for (int e : set) {
+            arr[a++] = e;
+        }
+        return arr;
+    }
+
+}
+
+/*
+Union: set of all distinct elements that are in any of these sets.
+Intersection: set of all distinct elements that are in all of these sets.
+ */
---
+++ src/main/java/leetcode/array/IntersectionOfTwoArraysII.java
@@ -0,0 +1,73 @@
+package leetcode.array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+// https://leetcode.com/problems/intersection-of-two-arrays/
+// https://leetcode.com/problems/intersection-of-two-arrays-ii/
+// duplicate elements are allowed
+public class IntersectionOfTwoArraysII {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(intersect(new int[]{1, 2, 3, 4, 5}, new int[]{2, 2, 3, 4, 5, 6})));
+    }
+
+
+    // TC: O(n + m)
+    // SC: O(n + m). O(m) for returning
+    public static int[] intersect(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int e : nums1) {
+            map.put(e, map.getOrDefault(e, 0) + 1);
+        }
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int e : nums2) {
+            if (map.containsKey(e) && map.get(e) > 0) {
+                list.add(e);
+                map.put(e, map.get(e) - 1);
+            }
+        }
+
+        int[] arr = new int[list.size()];
+        int a = 0;
+        for (int e : list) {
+            arr[a++] = e;
+        }
+        return arr;
+    }
+
+    // Follow Up
+    // What if the given array is already sorted.
+
+    // O(max(n, m)), O(n) for algorithm , O(n) for returning
+    public static int[] intersectII(int[] nums1, int[] nums2) {
+
+        int i = 0, j = 0;
+        ArrayList<Integer> list = new ArrayList<>();
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] == nums2[j]) {
+                list.add(nums1[i]);
+                i++;
+                j++;
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+
+        int[] arr = new int[list.size()];
+        int a = 0;
+        for (int e : list) {
+            arr[a++] = e;
+        }
+        return arr;
+    }
+
+
+
+}
